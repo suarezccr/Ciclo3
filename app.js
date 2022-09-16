c143198daf4a3cd346b6b3274cf443a68a32d2a7
suarezccr <cristian.suarez.mt@usa.edu.co>
2022-09-15 21:49:00 -0500

    mi primer version 1.0

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("Hello World");
+
+//var time= new Date();
+//alert (time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+//alert("esto es un mensaje en una ventana!");
+//document.write("la informacion se muestra dentro de la pagina web html");
+//document.write("<h3>Mi titulo nivel 3</h3>");
+
+//var num1=1;
+//var num2=2;
+//alert (num1+num2)
+//otras maneras de crear los scripts es directamente en el codigo html
+
+//en el Head y en el body
+
+// en el head:
+//var time = new Date();
+//console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+
+// en el body
+//let d = new Date();
+//document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + //d.getMinutes() + ":" + d.getSeconds()
+
+
+//diferencias entre let, var, y const 
+//https://cybmeta.com/var-let-y-const-en-javascript
+
+
+//ejemplo para validar un correo https://www.hostinger.co/tutoriales/insertar-javascript-en-html/  otro ejemplo https://developer.mozilla.org/es/docs/Learn/Getting_started_with_the_web/JavaScript_basics
